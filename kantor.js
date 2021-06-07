67673e15ee5fbebcecb955ce853ead3f8c1514c9
Klaudia S <st.klaudiaa@gmail.com>
2021-06-07 22:41:50 +0200

    daleted js-countButtonElement

--- kantor.js
+++ kantor.js
@@ -1,35 +1,36 @@
-let formElement = document.querySelector(".js-form");
-let formFirst = document.querySelector(".js-form__fieldFirst");
-let buttonCountElement = document.querySelector(".js-buttonCount");
-let buttonCleanElement = document.querySelector(".js-buttonClean");
-let eurElement = document.querySelector(".js-textEur");
-let usdElement = document.querySelector(".js-textUsd");
-let gbpElement = document.querySelector(".js-textGbp");
-let btcElement = document.querySelector(".js-textBtc");
-let chfElement = document.querySelector(".js-textChf");
+{
+    const formElement = document.querySelector(".js-form");
+    const formFirst = document.querySelector(".js-form__fieldFirst");
+    const buttonCleanElement = document.querySelector(".js-buttonClean");
+    const eurElement = document.querySelector(".js-textEur");
+    const usdElement = document.querySelector(".js-textUsd");
+    const gbpElement = document.querySelector(".js-textGbp");
+    const btcElement = document.querySelector(".js-textBtc");
+    const chfElement = document.querySelector(".js-textChf");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let first = +formFirst.value;
-    let eur = first / 4.5490;	
-    let usd = first / 3.7618;
-    let gbp = first / 5.2310;
-    let btc = first / 201159.24;
-    let chf = first / 4.1095;
-    
-    eurElement.innerText = eur.toFixed(2) + " €";
-    usdElement.innerText = usd.toFixed(2) + " $";
-    gbpElement.innerText = gbp.toFixed(2) + " £";
-    btcElement.innerText = btc.toFixed(2) + " ₿";
-    chfElement.innerText = chf.toFixed(2) + " ₣";
-});
+        const first = +formFirst.value;
+        const eur = first / 4.5490;
+        const usd = first / 3.7618;
+        const gbp = first / 5.2310;
+        const btc = first / 201159.24;
+        const chf = first / 4.1095;
+
+        eurElement.innerText = eur.toFixed(2) + " €";
+        usdElement.innerText = usd.toFixed(2) + " $";
+        gbpElement.innerText = gbp.toFixed(2) + " £";
+        btcElement.innerText = btc.toFixed(2) + " ₿";
+        chfElement.innerText = chf.toFixed(2) + " ₣";
+    });
 
 
-buttonCleanElement.addEventListener("click", () => {
-    eurElement.innerText = ("");
-    usdElement.innerText = ("");
-    gbpElement.innerText = ("");
-    btcElement.innerText = ("");
-    chfElement.innerText = ("");
-});
+    buttonCleanElement.addEventListener("click", () => {
+        eurElement.innerText = ("");
+        usdElement.innerText = ("");
+        gbpElement.innerText = ("");
+        btcElement.innerText = ("");
+        chfElement.innerText = ("");
+    });
+}
