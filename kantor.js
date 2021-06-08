9f7b69d0dd346daf870c9904d3cb3811ccdca879
Klaudia S <st.klaudiaa@gmail.com>
2021-06-08 10:46:14 +0200

    added function

--- kantor.js
+++ kantor.js
@@ -1,36 +1,43 @@
 {
-    const formElement = document.querySelector(".js-form");
-    const formFirst = document.querySelector(".js-form__fieldFirst");
-    const buttonCleanElement = document.querySelector(".js-buttonClean");
     const eurElement = document.querySelector(".js-textEur");
     const usdElement = document.querySelector(".js-textUsd");
     const gbpElement = document.querySelector(".js-textGbp");
     const btcElement = document.querySelector(".js-textBtc");
     const chfElement = document.querySelector(".js-textChf");
 
-    formElement.addEventListener("submit", (event) => {
+    const onFromElementSubmit = (event) => {
         event.preventDefault();
-
-        const first = +formFirst.value;
-        const eur = first / 4.5490;
-        const usd = first / 3.7618;
-        const gbp = first / 5.2310;
-        const btc = first / 201159.24;
-        const chf = first / 4.1095;
-
+        const formFirst = document.querySelector(".js-form__fieldFirst");
+        const amount = +formFirst.value;
+        const eur = amount / 4.5490;
+        const usd = amount / 3.7618;
+        const gbp = amount / 5.2310;
+        const btc = amount / 201159.24;
+        const chf = amount / 4.1095;
         eurElement.innerText = eur.toFixed(2) + " €";
         usdElement.innerText = usd.toFixed(2) + " $";
         gbpElement.innerText = gbp.toFixed(2) + " £";
         btcElement.innerText = btc.toFixed(2) + " ₿";
         chfElement.innerText = chf.toFixed(2) + " ₣";
-    });
+
+    };
 
 
-    buttonCleanElement.addEventListener("click", () => {
+    const OnButtonClean = () => {
         eurElement.innerText = ("");
         usdElement.innerText = ("");
         gbpElement.innerText = ("");
         btcElement.innerText = ("");
         chfElement.innerText = ("");
-    });
+
+    };
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const buttonCleanElement = document.querySelector(".js-buttonClean");
+        formElement.addEventListener("submit", onFromElementSubmit);
+        buttonCleanElement.addEventListener("click", OnButtonClean);
+
+    };
+
+    init();
 }
